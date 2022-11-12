86f7e999b76f5e854569014af8c7c94c9899cfa4
Stavros Tsompanidis <stavtsob@gmail.com>
2022-11-12 18:54:56 +0200

    fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,6 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img style="height:64px" src="{{ URL('/images/'.config('app.logo'))}}">
-                    {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
