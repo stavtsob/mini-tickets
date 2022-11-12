86b46715e04b6e4cb7750f23abd84ac24df5cedd
Stavros Tsompanidis <stavtsob@gmail.com>
2022-11-12 17:47:17 +0200

    small fix in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,7 +57,7 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
+                                @if (false && Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
