413111bf0ff8aa0f2ffee453a9d5478ba2d18122
Stavros Tsompanidis <stavtsob@gmail.com>
2022-11-12 18:50:44 +0200

    fix mobile menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img style="height:64px" src="{{ URL('/images/'.config('app.logo'))}}"
+                    <img style="height:64px" src="{{ URL('/images/'.config('app.logo'))}}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
