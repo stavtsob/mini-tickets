e34b0e3072cb65d9b23e58b1149acb49d1e84ee5
Stavros Tsompanidis <stavtsob@gmail.com>
2022-11-19 16:29:30 +0200

    filesystem config fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,7 +58,7 @@ return [
         'media' => [
             'driver' => 'local',
             'root'   => public_path('images/media'),
-            'url'    => env('APP_URL').'public/media',
+            'url'    => env('APP_URL').'/public/images/media',
         ],
 
     ],
