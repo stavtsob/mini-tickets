09304128a5af272ce7ec71a7ad7fd4d6694f3408
Stavros Tsompanidis <stavtsob@gmail.com>
2022-11-19 17:46:26 +0200

    media file permissions fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,7 +66,7 @@ return [
                 ],
                 'dir' => [
                     'public' => 0755,
-                    'private' => 0700,
+                    'private' => 0755,
                 ],
             ],
         ],
