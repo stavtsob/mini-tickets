05b7f2d696494d2784ce7ce302d69581b32fa045
Stavros Tsompanidis <stavtsob@gmail.com>
2022-11-19 17:44:01 +0200

    permissions test

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,6 +59,16 @@ return [
             'driver' => 'local',
             'root'   => public_path('images/media'),
             'url'    => env('APP_URL').'/public/images/media',
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
     ],
