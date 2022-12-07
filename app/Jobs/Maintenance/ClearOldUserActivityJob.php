088320450a8ebe90a4c3b67a52f597ad1c2fcc04
Stavros Tsompanidis <stavtsob@gmail.com>
2022-12-07 08:37:27 +0200

    Initiallize scheduler maintenance of database jobs

---
+++ app/Jobs/Maintenance/ClearOldUserActivityJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs\Maintenance;
+
+use App\Models\UserActivity;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ClearOldUserActivityJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $thirtyDaysBefore = Carbon::now()->subDays(30)->toDateTimeString();
+        UserActivity::where('created_at','<=',$thirtyDaysBefore)->delete();
+    }
+}
