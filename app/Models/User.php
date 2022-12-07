f2e213b76b18cab036a5fbe39bf123701fbeb62c
Stavros Tsompanidis <stavtsob@gmail.com>
2022-12-07 13:11:12 +0200

    order fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,6 @@ class User extends Authenticatable
 
     public function activities()
     {
-        return $this->hasMany(UserActivity::class);
+        return $this->hasMany(UserActivity::class)->orderBy('created_at', 'DESC');
     }
 }
